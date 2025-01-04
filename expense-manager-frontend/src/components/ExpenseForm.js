d89ddf350d9be8591c9102d5e62740cf722f9722
Anshuman Panda <anshumanp9920@gmail.com>
2025-01-04 13:12:34 +0530

    Implement ExpenseForm component with validation using Formik and Yup; update Header component styling

--- expense-manager-frontend/src/components/ExpenseForm.js
+++ expense-manager-frontend/src/components/ExpenseForm.js
@@ -1,2 +1,61 @@
-const ExpenseForm = () => <h1>Expense Manager</h1>;
+import * as Yup from 'yup';
+import Formik, { ErrorMessage, Field } from 'formik';
+
+const ExpenseForm = ({ onSaveExpense }) =>{
+
+    const initialValues = {
+        title: '',
+        amount: '',
+        category: '',
+        date: '',
+    };
+
+    const validationSchema = Yup.object({
+        title: Yup.string().required('Title is required.'),
+        amount: Yup.number().required('Amount is required').positive('Amount must be positive'),
+        category: Yup.string().required('Category is required'),
+        date: Yup.date().required('Date is required')
+    });
+    
+    const handleSubmit = (values, { resetForm }) => {
+        onSaveExpense(values);
+        resetForm();
+    };
+
+    return(
+        <Formik 
+            initialValues = {initialValues}
+            validationSchema = {validationSchema}
+            onSubmit = {handleSubmit}
+        >
+            {() => (
+                <Form>
+                    <div>
+                        <label>Title</label>
+                        <Field type="text" name="title" />
+                        <ErrorMessage name='title' component='div' style={{ color: 'red'}} />
+                    </div>
+                    <div> 
+                        <label>Amount</label>
+                        <Field type="number" name="amount" />
+                        <ErrorMessage name='amount' component="div" style={{ color: 'red'}}/>
+                    </div>
+                    <div>
+                        <label>Category</label>
+                        <Field type="text" name="category" />
+                        <ErrorMessage name='category' component="div" style ={{ color: 'red'}} />
+                    </div>
+                    <div> 
+                        <label>Date</label>
+                        <Field type="date" name="date"/>
+                        <ErrorMessage name='date' component="div" style={{ color:'red'}}/>
+                    </div>
+
+                    <button type='submit'>Add Expense</button>
+                </Form>
+
+            )}
+        </Formik>
+    );
+} 
 export default ExpenseForm;
--- expense-manager-frontend/src/components/Header.js
+++ expense-manager-frontend/src/components/Header.js
@@ -1,2 +1,8 @@
-const Header = () => <h1>Expense Manager</h1>;
+const Header = () =>{
+    return(
+        <header>
+            <h1>Expense Manager</h1>
+        </header>
+    );
+}
 export default Header;
